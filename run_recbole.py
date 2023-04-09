c59ad1b2b91324f879f6a1e3ee05cf30934a2866
gkoren2 <guy.koren@intel.com>
2023-04-09 05:08:28 -0700

    adding seed from cmd line and updating the EDA notebook

--- run_recbole.py
+++ run_recbole.py
@@ -16,12 +16,12 @@ from recbole.quick_start import run_recbole
 if __name__ == '__main__':
     begin = time.time()
     parameter_dict = {
-        'neg_sampling': None
+        'neg_sampling': None,
         # 'gpu_id':3,
         # 'attribute_predictor':'not',
         # 'attribute_hidden_size':"[256]",
         # 'fusion_type':'gate',
-        # 'seed':212,
+        'seed':212,
         # 'n_layers':4,
         # 'n_heads':1
     }
@@ -29,9 +29,10 @@ if __name__ == '__main__':
     parser.add_argument('--model', '-m', type=str, default='SASRecD', help='name of models')
     parser.add_argument('--dataset', '-d', type=str, default='Amazon_Beauty', help='name of datasets')
     parser.add_argument('--config_files', type=str, default='configs/Amazon_Beauty.yaml', help='config files')
+    parser.add_argument('--seed', type=int, default=212, help='seed')
 
     args, _ = parser.parse_known_args()
-
+    parameter_dict['seed']=args.seed
     config_file_list = args.config_files.strip().split(' ') if args.config_files else None
     run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list,config_dict=parameter_dict)
     end=time.time()
